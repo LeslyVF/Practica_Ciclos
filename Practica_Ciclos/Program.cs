5e6e8434c28c91a9c98b86409f52de03031b586a
armentaspcs <armentaspcs@DESKTOP-AHUAJBN>
2022-10-21 15:34:42 -0700

    Agregar archivos de proyecto.

---
+++ Practica_Ciclos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Ciclos
+{
+    class Program
+    {
+        static double exponencial(int a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+        static void Main(string[] args)
+        {
+            
+            int exponente, b; 
+                int n;
+
+            Console.Write("favor de ingresar el valor de n:");
+            n = int.Parse(Console.ReadLine());
+
+            for(int x = 0; x < n; x++)
+               {
+                Console.Write("Favor de ingresar la base:");
+                b = int.Parse(Console.ReadLine());
+
+                Console.Write("Favor de ingresar el exponente:");
+                exponente = int.Parse(Console.ReadLine());
+
+                Console.Write(b + "Evelado a:" + exponente + " = " + exponencial(b, exponente));
+               }
+            Console.ReadKey();
+        }
+    }
+}
